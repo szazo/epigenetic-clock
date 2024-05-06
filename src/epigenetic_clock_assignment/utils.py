4fbcafcacc02681faa1ec3fafbe646ab12d438be
Zoltán Szarvas <zoltan.szarvas@neting.hu>
2024-05-06 10:33:54 +0200

    feat: download_file now supports zip

--- src/epigenetic_clock_assignment/utils.py
+++ src/epigenetic_clock_assignment/utils.py
@@ -1,40 +1,57 @@
-from typing import Optional
+from typing import Optional, Union, Literal
 import logging
 import os
 import wget
 import gzip
+import zipfile
 import shutil
 
 
 def download_file(url: str,
                   out_filepath: str,
-                  intermediate_gzip_filepath: Optional[str] = None):
+                  intermediate_archive_filepath: Optional[str] = None,
+                  archive_format: Optional[Union[Literal['zip'],
+                                                 Literal['gzip']]] = None):
     log = logging.getLogger('download_file')
 
     if not os.path.exists(out_filepath):
-        log.debug(f'file {out_filepath} does not exists; downloading...')
+        log.debug(f'file {out_filepath} does not exists; need to download')
 
-        os.makedirs(os.path.dirname(out_filepath), exist_ok=True)
+        out_dir = os.path.dirname(out_filepath)
+        log.debug(f'creating directory "{out_dir}" if does not exists...')
+        os.makedirs(out_dir, exist_ok=True)
 
-        if intermediate_gzip_filepath is not None:
+        if intermediate_archive_filepath is not None:
 
-            if os.path.exists(intermediate_gzip_filepath):
+            if os.path.exists(intermediate_archive_filepath):
                 # delete before download
-                os.remove(intermediate_gzip_filepath)
-            os.makedirs(os.path.dirname(intermediate_gzip_filepath),
-                        exist_ok=True)
+                os.remove(intermediate_archive_filepath)
 
-        wget.download(url=url, out=intermediate_gzip_filepath or out_filepath)
+            intermediate_archive_dir = os.path.dirname(
+                intermediate_archive_filepath)
+            os.makedirs(intermediate_archive_dir, exist_ok=True)
 
-        if intermediate_gzip_filepath is not None:
-            log.debug(f'unzipping {intermediate_gzip_filepath}...')
+        log.debug(f'downloading...')
+        wget.download(url=url,
+                      out=intermediate_archive_filepath or out_filepath)
 
-            with gzip.open(intermediate_gzip_filepath, 'rb') as infile:
-                with open(out_filepath, 'wb') as outfile:
-                    shutil.copyfileobj(infile, outfile)
+        if intermediate_archive_filepath is not None:
+            log.debug(f'ungzipping {intermediate_archive_filepath}...')
+
+            if archive_format == 'gzip':
+                with gzip.open(intermediate_archive_filepath, 'rb') as infile:
+                    with open(out_filepath, 'wb') as outfile:
+                        shutil.copyfileobj(infile, outfile)
+            elif archive_format == 'zip':
+                log.debug(f'unzipping {intermediate_archive_filepath}...')
+
+                with zipfile.ZipFile(intermediate_archive_filepath) as zf:
+                    zf.extract(os.path.basename(out_filepath), out_dir)
+            else:
+                raise Exception(f'invalid archive format: {archive_format}')
 
             # delete intermediate file after gunzip
-            os.remove(intermediate_gzip_filepath)
+            os.remove(intermediate_archive_filepath)
 
         log.debug(f'file {out_filepath} ready')
     else:
